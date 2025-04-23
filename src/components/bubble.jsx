6f7cd81bb4bb363dbe7575ed9cdb26bd8c2f36a9
gagan-0108 <yadavgagan61@gmail.com>
2025-04-23 05:27:48 +0000

    ୨ৎ

--- src/components/bubble.jsx
+++ src/components/bubble.jsx
@@ -3,20 +3,16 @@ import { MeshDistortMaterial, Sphere } from "@react-three/drei";
 const Shape = () => {
   return (
     <>
-      <Sphere args={[1, 100, 200]} scale={2.8}>
+      <Sphere args={[1, 100, 200]} scale={2.4}>
         <MeshDistortMaterial
-          color="#e5e5f0"
+          color="#d9ecff"
           attach="material"
-          distort={0.3}
-          speed={1.5}
-          roughness={0.4}
-          metalness={0.1}
-          opacity={0.9}
-          transparent
+          distort={0.5}
+          speed={2}
         />
       </Sphere>
-      <ambientLight intensity={0.8} />
-      <directionalLight position={[2, 2, 2]} intensity={1} />
+      <ambientLight intensity={2} />
+      <directionalLight position={[1, 2, 3]} />
     </>
   );
 };
